75d8eedf777976bf48ebe46175f1d3b88d59fc0f
nathanchoui <nathan_choui@163.com>
2018-07-31 20:42:57 +0800

    amqp init

---
+++ kafka-study/src/main/java/personal/nathan/KafkaApplication.java
@@ -0,0 +1,17 @@
+package personal.nathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ * <p>
+ * Created by nathan.z on 2018/7/29.
+ */
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
